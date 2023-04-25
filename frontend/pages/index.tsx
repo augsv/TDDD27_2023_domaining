b1ed4afd89cd2600565d17d84b20bfbd52608b90
august <augsv102@student.liu.se>
2023-04-25 23:46:48 +0200

    Removed unused dependencies

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Layout from '@/components/layout'
 import Hero from '@/components/index/hero'
 
 const inter = Inter({ subsets: ['latin'] })
