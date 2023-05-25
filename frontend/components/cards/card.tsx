d119dd14ec9162ae512ce352387e7b6cb5388630
august <augsv102@student.liu.se>
2023-05-25 14:31:23 +0200

    Added Link to product page on cards

--- frontend/components/cards/card.tsx
+++ frontend/components/cards/card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Domain } from "@/types/domain";
 import { FC, ReactNode } from "react";
 
@@ -7,18 +8,20 @@ interface CardProps {
 }
 
 const Card: FC<CardProps> = ({ domain }) => {
-    const width = 500
-    const height = 500
+    const width = 500;
+    const height = 500;
+
+    const link = "/doman/" + domain.name.split('.')[0];
 
     return (
         <>
-            <a href="#" className="group">
+            <Link href={link} className="group">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                     <Image className="h-full w-full object-cover object-center group-hover:opacity-75" width={width} height={height} src="/image-1.jpg" alt="" />
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">{ domain.name }</h3>
                 <p className="mt-1 text-lg font-medium text-gray-900">{ domain.price + " " + domain.currency }</p>
-            </a>
+            </Link>
         </>
     );
 };
