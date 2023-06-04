33e0427c2d000d664216e8e44f16a957d9c845a0
august <augsv102@student.liu.se>
2023-06-04 22:25:42 +0200

    Removed stub domains data

--- frontend/components/lists/domain.tsx
+++ frontend/components/lists/domain.tsx
@@ -3,7 +3,6 @@ import Card from "../cards/card";
 import { DomainArray } from '@/types/domain';
 
 const DomainList: React.FC<DomainArray> = ({ domains }) => {
-
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
